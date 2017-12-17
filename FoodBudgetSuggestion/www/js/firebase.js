0320acf9c46d783618e6404a6547563d8f8dbc94
philliprios <pipoyarman@gmail.com>
2017-12-17 23:43:37 +0800

    add material design to the app and putting firebase

---
+++ FoodBudgetSuggestion/www/js/firebase.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyDnGe4xr7c2UqcEomBBBEM9pCK5F5J7JGA",
+    authDomain: "food-budget-suggestion-app.firebaseapp.com",
+    databaseURL: "https://food-budget-suggestion-app.firebaseio.com",
+    projectId: "food-budget-suggestion-app",
+    storageBucket: "food-budget-suggestion-app.appspot.com",
+    messagingSenderId: "75561022399"
+  };
+  
+firebase.initializeApp(config);
